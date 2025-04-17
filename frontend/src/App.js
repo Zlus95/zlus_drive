678e60bdaca59398b939cc8599088bb2e8b4285f
APavlov <sologod@yandex.ru>
2025-04-17 16:37:17 +0300

    fix style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,11 @@
 import "./index.css";
 
 function App() {
-  return <div className="text-red-500">aaaa</div>;
+  return (
+    <div className="text-red-500">
+      <div>aaaa</div>
+    </div>
+  );
 }
 
 export default App;
