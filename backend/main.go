a9e204ac960b3e290991478602f253e9c6a7cc60
APavlov <sologod@yandex.ru>
2025-04-18 15:09:30 +0300

    create cors middleware

--- backend/main.go
+++ backend/main.go
@@ -20,21 +20,7 @@ func main() {
 
 	r := gin.Default()
 
-	// Настройка CORS
-	r.Use(func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
-		}
-
-		c.Next()
-	})
-
+	r.Use(middleware.DevelopmentCORS())
 	r.Use(middleware.RegMiddlware())
 	r.Use(middleware.LoginMiddlware())
 
