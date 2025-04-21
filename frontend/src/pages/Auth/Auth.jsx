100d0a80136489e96c1fb04e27e0d495cab00614
APavlov <sologod@yandex.ru>
2025-04-21 11:00:24 +0300

    login form

---
+++ frontend/src/pages/Auth/Auth.jsx
@@ -0,0 +1,88 @@
+import React, { useState, useRef } from "react";
+import api from "../../api";
+import { Link, useNavigate } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
+
+async function login(user) {
+  const { data } = await api.post("/login", user);
+  localStorage.setItem("token", data.token);
+  return data;
+}
+
+const Auth = () => {
+  const navigate = useNavigate();
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+  const [validForm, setValid] = useState(false);
+
+  const changeInput = () => {
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    setValid(email.trim() !== "" && password.trim() !== "");
+  };
+
+  const mutationLogin = useMutation({
+    mutationFn: login,
+    onSuccess: () => navigate("/rooms"),
+    onError: ({ response }) => alert(response.data),
+  });
+
+  const handlerSubmit = (event) => {
+    event.preventDefault();
+    if (emailRef.current && passwordRef.current) {
+      const email = emailRef.current.value;
+      const password = passwordRef.current.value;
+      mutationLogin.mutate({ email, password });
+    }
+  };
+
+  return (
+    <form
+      className="flex justify-center items-center h-full"
+      onSubmit={handlerSubmit}
+    >
+      <div className="h-80 w-72 bg-black/50 border-2 border-neutral-400 rounded">
+        <div className="text-primary flex justify-center pt-4 text-lg">
+          Chat
+        </div>
+        <div className="flex flex-col gap-2 p-4">
+          <label htmlFor="email" className="text-white/50">
+            Email:
+          </label>
+          <input
+            placeholder="login"
+            id="email"
+            onChange={changeInput}
+            ref={emailRef}
+          />
+          <label htmlFor="password" className="text-white/50">
+            Password:
+          </label>
+          <input
+            placeholder="password"
+            type="password"
+            id="password"
+            ref={passwordRef}
+            onChange={changeInput}
+          />
+        </div>
+        <div className="flex justify-center pt-4">
+          <button
+            className={validForm ? "text-primary" : "text-primary/50"}
+            disabled={!validForm}
+          >
+            Sign in
+          </button>
+        </div>
+        <div className="p-4 flex gap-2">
+          <p className="text-white/50">Don't have an account?</p>
+          <Link to="/register" className="text-primary">
+            Sign up
+          </Link>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default Auth;
