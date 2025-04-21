6fc07477b113e7928c8f929315c441fa594caef1
APavlov <sologod@yandex.ru>
2025-04-21 16:17:16 +0300

    file middlwares

--- backend/middleware/file.go
+++ backend/middleware/file.go
@@ -1,8 +1,26 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"path/filepath"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
 
 const maxFileSize = 5 << 20 // fix the size later
+
+var forbiddenMimeTypes = map[string]bool{
+	"application/x-msdownload":      true,
+	"application/x-sh":              true,
+	"application/x-php":             true,
+	"application/javascript":        true,
+	"application/x-bat":             true,
+	"application/x-msdos-program":   true,
+	"application/vnd.ms-powerpoint": true,
+	"application/xhtml+xml":         true,
+	"application/octet-stream":      true,
+}
 
 func FileSizeMiddlWare(c *gin.Context) {
 	file, header, err := c.Request.FormFile("file")
@@ -19,4 +37,51 @@ func FileSizeMiddlWare(c *gin.Context) {
 	}
 
 	c.Next()
+}
+
+func MineFile(c *gin.Context) {
+	file, header, err := c.Request.FormFile("file")
+
+	if err != nil {
+		c.AbortWithStatusJSON(400, gin.H{"error": "File is required"})
+		return
+	}
+
+	defer file.Close()
+
+	if isHasForbidden(header.Filename) {
+		c.AbortWithStatusJSON(400, gin.H{"error": "File extension not allowed"})
+		return
+	}
+
+	buffer := make([]byte, 512)
+	_, err = file.Read(buffer)
+
+	if err != nil {
+		c.AbortWithStatusJSON(400, gin.H{"error": "Invalid file"})
+		return
+	}
+
+	file.Seek(0, 0)
+
+	mineType := http.DetectContentType(buffer)
+
+	if forbiddenMimeTypes[mineType] {
+		c.AbortWithStatusJSON(400, gin.H{"error": "File type not allowed"})
+		return
+	}
+
+	c.Next()
+}
+
+func isHasForbidden(fileName string) bool {
+	forbidden := []string{".exe", ".js", ".php", ".sh", ".bat", ".com", ".html", ".svg"}
+	ext := strings.ToLower(filepath.Ext(fileName))
+
+	for _, file := range forbidden {
+		if ext == file {
+			return true
+		}
+	}
+	return false
 }
