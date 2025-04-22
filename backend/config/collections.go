814d6180938887dadc3d643e315569942852de8f
APavlov <sologod@yandex.ru>
2025-04-22 13:40:23 +0300

    files collection

--- backend/config/collections.go
+++ backend/config/collections.go
@@ -3,9 +3,11 @@ package config
 import "go.mongodb.org/mongo-driver/mongo"
 
 var (
-	UserCollection *mongo.Collection
+	UserCollection *mongo.Collection;
+	FilesCollection *mongo.Collection
 )
 
 func InitCollections(db *mongo.Database) {
 	UserCollection = db.Collection("users")
+	FilesCollection = db.Collection("files")
 }
