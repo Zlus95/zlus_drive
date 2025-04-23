ede538e876226823517ecdabe72f95d0ec22debc
APavlov <sologod@yandex.ru>
2025-04-23 14:11:23 +0300

    home page

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import Header from "../../ui-kit/Header";
+import api from "../../api";
+import { useQuery } from "@tanstack/react-query";
+
+function useGetFiles() {
+  return useQuery({
+    queryKey: ["filesList"],
+    refetchOnWindowFocus: false,
+    queryFn: async () => {
+      const response = await api.get("/files");
+      return response.data;
+    },
+  });
+}
 
 const Home = () => {
-  console.log(1);
+  const { isLoading, data, error, isError, isSuccess } = useGetFiles();
 
   return (
     <div>
