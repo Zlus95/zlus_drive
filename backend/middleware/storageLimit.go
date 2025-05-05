68c5713593fc1930c504f49b08566d0442854f1d
APavlov <sologod@yandex.ru>
2025-05-05 16:05:41 +0300

    fix storage limit func

--- backend/middleware/storageLimit.go
+++ backend/middleware/storageLimit.go
@@ -25,7 +25,13 @@ func StorageLimitMiddlware() gin.HandlerFunc {
 			return
 		}
 
-		c.Set(StorageLimitContextKey, user)
+		if user.StorageLimit > 15 * 1024 * 1024 * 1024 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Storage Limit must be less than 15GB"})
+			c.Abort()
+			return
+		}
+
+		c.Set(StorageLimitContextKey, user.StorageLimit)
 
 		c.Next()
 	}
